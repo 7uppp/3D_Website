342a861ee949c869a95e49256b56808ddccdb00f
7uppp <markwang9315@gmail.com>
2023-03-08 10:46:07 +1000

    update

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
 
+//这是一个自定义高阶组件，用来包裹每个页面的section，以便实现滚动动画
 const SectionWrapper = (Component, idName) =>
   function HOC() {
     return (
